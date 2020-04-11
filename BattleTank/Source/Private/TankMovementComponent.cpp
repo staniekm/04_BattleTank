f68453341dd23eacf4b75f652775ec49a9d18c3e
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-11 13:50:18 +0200

    Finished manual tank movement

--- BattleTank/Source/Private/TankMovementComponent.cpp
+++ BattleTank/Source/Private/TankMovementComponent.cpp
@@ -3,21 +3,29 @@
 #include "TankTrack.h"
 #include "TankMovementComponent.h"
 
-void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+void UTankMovementComponent::Initialise(UTankTrack *LeftTrackToSet, UTankTrack *RightTrackToSet)
 {
-    if(!LeftTrackToSet || !RightTrackToSet)
-    {
-        LeftTrack = LeftTrackToSet;
-        RightTrack = RightTrackToSet;
-    }
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
 }
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-    //auto Name = GetName();
-    UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
-
+    if (!LeftTrack || !RightTrack)
+    {
+        return;
+    }
     LeftTrack->SetThrottle(Throw);
     RightTrack->SetThrottle(Throw);
     // TODO prevet double speed
-    
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+    if (!LeftTrack || !RightTrack)
+    {
+        return;
+    }
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(-Throw);
+    // TODO prevet double speed
 }
--- BattleTank/Source/Public/TankMovementComponent.h
+++ BattleTank/Source/Public/TankMovementComponent.h
@@ -22,7 +22,10 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
 
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
+
 private:
-	UTankTrack* LeftTrack = nullptr;
-	UTankTrack* RightTrack = nullptr;
+	UTankTrack *LeftTrack = nullptr;
+	UTankTrack *RightTrack = nullptr;
 };
