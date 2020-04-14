cacd04b63279945eafd509031b5125f0ce9180ec
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-14 18:05:37 +0200

    Crash fixed
    *!TankAimingComponent

--- BattleTank/Source/Private/Tank.cpp
+++ BattleTank/Source/Private/Tank.cpp
@@ -12,12 +12,14 @@ ATank::ATank()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-
-
 }
 
 void ATank::AimAt(FVector HitLocation)
 {
+	if (!TankAimingComponent)
+	{
+		return;
+	}
 	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
 
