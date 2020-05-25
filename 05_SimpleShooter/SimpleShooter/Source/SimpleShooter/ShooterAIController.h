d294fa8bc18e82e7da2f443b0c951cc9f72a870a
Marcin Staniek <marcin.staniek97@gmail.com>
2020-05-25 13:11:57 +0200

    Create and Setup an AI controller

---
+++ 05_SimpleShooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ShooterAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API AShooterAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
